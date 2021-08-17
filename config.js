79743e86273e4428773dd966480d431ab4208622
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2021-08-17 19:15:20 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+  bgLink: 'https://c4.wallpaperflare.com/wallpaper/880/299/772/tree-heart-hearts-love-wallpaper-preview.jpg', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: './img/avt.jpg', //link ảnh lookMe
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
