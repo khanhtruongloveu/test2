e8bb6d04b7bd4cc05835c0f5ac7e3c20f41cccd9
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2021-08-17 19:19:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
   bgLink: 'https://c4.wallpaperflare.com/wallpaper/880/299/772/tree-heart-hearts-love-wallpaper-preview.jpg', //link ảnh nền
     iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+      background:'https://c4.wallpaperflare.com/wallpaper/880/299/772/tree-heart-hearts-love-wallpaper-preview.jpg', //link ảnh nền
     
